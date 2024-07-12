fbac9393ba3f4fe4a6f5f735b4fb864b0bf46143
DraganDjuza <dragan.djuza@centogene.com>
2024-07-12 19:15:51 +0200

    added libreries, request and BeautifulSoup

---
+++ app.py
@@ -0,0 +1,11 @@
+# import requests
+# import BeautifulSoup
+
+
+# url = "https://en.wikipedia.org/wiki/States_and_union_territories_of_India"
+# headers = {}  #To get header; google search 'what is my user agent'
+# search_response=requests.get(url,headers = headers)
+# if search_response.status_code == 200:
+#     soup=BeautifulSoup(search_response.content,'html.parser')
+
+# soup.find('h1',{'id':'firstHeading'}).get_text()
